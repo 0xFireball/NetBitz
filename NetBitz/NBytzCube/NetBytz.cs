1fe2936ace6ac4c60974394a62a5cad2ad5e9fc9
0xFireball <0xFireball@outlook.com>
2016-03-09 17:45:38 -0800

    improve some code, but it doesn't work multifile yet

--- NetBitz/NBytzCube/NetBytz.cs
+++ NetBitz/NBytzCube/NetBytz.cs
@@ -11,6 +11,7 @@ namespace NBytzCube
 {
     public class NBCube
     {
+        static List<Assembly> loadedAssemblies;
         public static void LaunchAssembly(byte[] assemblyContents)
         {
             if (assemblyContents.Length == 0)
@@ -37,6 +38,7 @@ namespace NBytzCube
         }
         public static void ExtractUnpackAndLaunchAssembly(string compressedAssemblyContents)
         {
+            loadedAssemblies = new List<Assembly>();
 #if DEBUG
             Console.WriteLine("Decompressing...");
 #endif
@@ -44,22 +46,49 @@ namespace NBytzCube
             GC.Collect();
             GC.WaitForPendingFinalizers(); //Clean up the massive memory usage
             string[] squashedAssemblies = decompressedAssemblyContents.Split('_').Reverse().ToArray();
-            AppDomain currentDomain = AppDomain.CurrentDomain;
+            //AppDomain packerDomain = AppDomain.CreateDomain("NBPackerLaunch");
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += PackerDomain_ReflectionOnlyAssemblyResolve;
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
 #if DEBUG
             Console.WriteLine("Unpacking...");
 #endif
             foreach (var sqAsm in squashedAssemblies.Take(squashedAssemblies.Length - 2)) //All except the last
             {
-                currentDomain.Load(Convert.FromBase64String(sqAsm));
+                loadedAssemblies.Add(Assembly.ReflectionOnlyLoad(Convert.FromBase64String(sqAsm)));
             }
 #if DEBUG
             Console.WriteLine("Initializing...");
 #endif
             var executableAsm = squashedAssemblies[squashedAssemblies.Length - 1];
+            Assembly asm = Assembly.Load(Convert.FromBase64String(executableAsm));
+            /*
+            foreach (Assembly asm in packerDomain.GetAssemblies().Reverse()) //Find the EXE and run it!
+            {
+                if (asm.EntryPoint != null)
+                {
+                    MethodInfo main = asm.EntryPoint;
+                    var defaultParameters = main.GetParameters().Select(p => GetDefaultValue(p.ParameterType)).ToArray();
+                    main.Invoke(null, defaultParameters);
+                }
+            }
+            
             Assembly asm = Assembly.Load(Convert.FromBase64String(executableAsm)); //The last one, the executable Assembly
+            //currentDomain.Load(Convert.FromBase64String(executableAsm));
+            */
             MethodInfo main = asm.EntryPoint;
             var defaultParameters = main.GetParameters().Select(p => GetDefaultValue(p.ParameterType)).ToArray();
             main.Invoke(null, defaultParameters);
+
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return loadedAssemblies.Where(asm => asm.FullName == args.RequestingAssembly.FullName).ElementAt(0); //load the assembly from the list
+        }
+
+        private static Assembly PackerDomain_ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return System.Reflection.Assembly.ReflectionOnlyLoad(args.Name);
         }
     }
 }
